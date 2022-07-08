6be394987408561eea1ecf866c1cbdb17eb9da2d
Karkusha14 <andrewmash17@gmail.com>
2022-07-09 00:01:51 +0300

    first commit

--- tel_bot.py
+++ tel_bot.py
@@ -9,24 +9,35 @@ def get_weather(city, open_weather_token):
     code_to_emoji ={
         'Clear': 'Ясно \U00002600',
         'Clouds': 'Облачно \U00002601',
-        'Rain': 'Дождь \U00001F327'
+        'Rain': 'Дождь \U0001F327',
+        'Drizzle': 'Дождь \U0001F327',
+        'Thundershtorm': 'Гроза \U0001F329',
+        'Snow': 'Снег \U0001F328',
+        'Mist': 'Туман \U0001F32B'
     }
     try:
         r = requests.get(
             f'https://api.openweathermap.org/data/2.5/weather?q={city}&appid={open_weather_token}&units=metric'
         )
         data = r.json()
-        #  pprint(data)
+        pprint(data)
 
         city = data['name']
         current_temp = data['main']['temp']
+
+        weather_emoji = data['weather'][0]['main']
+        if weather_emoji in code_to_emoji:
+            we = code_to_emoji[weather_emoji]
+        else:
+            we = 'Нету смайлика'
+
         humidity = data['main']['humidity']
         pressure = data['main']['pressure']
         sunrise_timestamp = datetime.datetime.fromtimestamp(data['sys']['sunrise'])
         sunset_timestamp = datetime.datetime.fromtimestamp(data['sys']['sunset'])
         length_of_day = sunset_timestamp - sunrise_timestamp
-        print(f'Погода в городе: {city}\nТемпература:{current_temp}°С\n' 
-              f'Влажность:{humidity}\nДавление: {pressure}мм.рт.столба\n'
+        print(f'Погода в городе: {city}\nТемпература:{current_temp}°С {we}\n' 
+              f'Влажность:{humidity}\nДавление: {(int(pressure) * 0,750063755419211)}мм.рт.столба\n'
               f'Восход солнца: {sunrise_timestamp} Закат солнца : {sunset_timestamp}\n'
               f'Длинна дня {length_of_day}')
     except Exception as ex:
