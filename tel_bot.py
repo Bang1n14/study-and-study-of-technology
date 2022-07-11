5d6e69be9cb09e608006676c49be870c5bd9b6b2
Karkusha14 <andrewmash17@gmail.com>
2022-07-11 22:00:34 +0300

    Create tl_bot

--- tel_bot.py
+++ tel_bot.py
@@ -1,12 +1,25 @@
-from config import open_weather_token
-from pprint import pprint
+"""Based from https://www.youtube.com/watch?v=fa1FUW1jLAE&t=53s """
+
+
 import requests
 import datetime
+from config import tg_bot_token, open_weather_token
+from aiogram import Bot, types
+from aiogram.dispatcher import Dispatcher
+from aiogram.utils import executor
+
+bot = Bot(token=tg_bot_token)
+dp = Dispatcher(bot)
 
 
-def get_weather(city, open_weather_token):
+@dp.message_handler(commands=['start'])
+async def start_command(message: types.Message):
+    await message.reply('Скажи в каком городе ты хочешь узнать какая сейчас погода ')
 
-    code_to_emoji ={
+
+@dp.message_handler()
+async def get_weather(message: types.Message):
+    code_to_emoji = {
         'Clear': 'Ясно \U00002600',
         'Clouds': 'Облачно \U00002601',
         'Rain': 'Дождь \U0001F327',
@@ -17,10 +30,9 @@ def get_weather(city, open_weather_token):
     }
     try:
         r = requests.get(
-            f'https://api.openweathermap.org/data/2.5/weather?q={city}&appid={open_weather_token}&units=metric'
+            f'https://api.openweathermap.org/data/2.5/weather?q={message.text}&appid={open_weather_token}&units=metric'
         )
         data = r.json()
-        pprint(data)
 
         city = data['name']
         current_temp = data['main']['temp']
@@ -37,21 +49,14 @@ def get_weather(city, open_weather_token):
         sunset_timestamp = datetime.datetime.fromtimestamp(data['sys']['sunset'])
         length_of_day = sunset_timestamp - sunrise_timestamp
 
-        print(f'Погода в городе: {city}\nТемпература:{current_temp}°С {we}\n' 
-              f'Влажность: {humidity}%\nДавление: {int(pressure)}мм.рт.столба\n'
-              f'Восход солнца: {sunrise_timestamp}\nЗакат солнца : {sunset_timestamp}\n'
-              f'Длинна дня {length_of_day}')
+        await message.reply(f'Погода в городе: {city}\nТемпература:{current_temp}°С {we}\n'
+                            f'Влажность: {humidity}%\nДавление: {int(pressure)}мм.рт.столба\n'
+                            f'Восход солнца: {sunrise_timestamp}\nЗакат солнца : {sunset_timestamp}\n'
+                            f'Длина дня {length_of_day}')
+
     except Exception as ex:
-        print(ex)
-        print('Wrong name place')
-
-
-
-
-def main():
-    city = input('Enter your place\n')
-    get_weather(city, open_weather_token)
+        await message.reply('\U0001F31AПроверьте написание города\U0001F31A')
 
 
 if __name__ == '__main__':
-    main()
+    executor.start_polling(dp)
